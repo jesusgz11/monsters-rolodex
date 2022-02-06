d44407e2e02446734a6ac5ea1d1d72b69564c018
Jesus Guzman <jesusguzman.0311@gmail.com>
2022-02-05 18:21:30 -0600

    Updates

---
+++ precache-manifest.b6d0bda872aeccc46e35180b2e0b55d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646586b00ca12866dabe20acd687756f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "20cffb4cb737238afcb6",
+    "url": "/monsters-rolodex/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "c8e56e1d6cf27670d4ed",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20cffb4cb737238afcb6",
+    "url": "/monsters-rolodex/static/js/main.581c8811.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
